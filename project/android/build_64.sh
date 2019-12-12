1d5307c6e804541d89e80288d4d4a20c451d5681
riddick <gaoruidong@insta360.com>
2019-12-12 17:05:51 +0800

    add gpu backends on android

--- project/android/build_64.sh
+++ project/android/build_64.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cmake ../../../ \
 -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake \
- -DCMAKE_BUILD_TYPE=Release \
- -DMNN_DEBUG=false \
+-DCMAKE_BUILD_TYPE=Release \
+-DMNN_DEBUG=false \
 -DANDROID_ABI="arm64-v8a" \
 -DANDROID_STL=c++_static \
 -DANDROID_NATIVE_API_LEVEL=android-21  \
 -DMNN_DEBUG=false \
+-DMNN_OPENCL=true \
+-DMNN_OPENGL=true \
+-DMNN_VULKAN=true \
 -DMNN_BUILD_FOR_ANDROID_COMMAND=true \
 -DNATIVE_LIBRARY_OUTPUT=. $1 $2
 
