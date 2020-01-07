a40dd07bd2ee3a982bc4df406f2acadfb4c4c8c6
海境 <haijing.zhs@alibaba-inc.com>
2020-01-07 18:05:59 +0800

    Add simulator to buildiOS.sh

--- project/ios/buildiOS.sh
+++ project/ios/buildiOS.sh
@@ -3,51 +3,54 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd ${SCRIPT_DIR}
 
-rm -rf iOSCOMBINED
-mkdir iOSCOMBINED
-cd iOSCOMBINED
-cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../..//cmake/ios.toolchain.cmake -DMNN_METAL=ON -DPLATFORM=OS -DENABLE_BITCODE=0 -G Xcode
-echo "Building for iOS Device"
+rm -rf iOS_64
+mkdir iOS_64
+cd iOS_64
+cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../../cmake/ios.toolchain.cmake -DMNN_METAL=ON -DIOS_ARCH="arm64" -DMNN_AAPL_FMWK=ON -DENABLE_BITCODE=0 -G Xcode
+echo "Building AArch64"
 xcodebuild ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -configuration Release -scheme MNN -target MNN -sdk iphoneos -quiet
 cd ../
 
-rm -rf iOSSIMULATOR64
-mkdir iOSSIMULATOR64
-cd iOSSIMULATOR64
-cmake ../../..// -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../..//cmake/ios.toolchain.cmake -DMNN_METAL=ON -DPLATFORM=SIMULATOR64 -DENABLE_BITCODE=0 -G Xcode
-echo "Building for 64Bit Simulator"
+rm -rf iOS_32
+mkdir iOS_32
+cd iOS_32
+cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../../cmake/ios.toolchain.cmake -DMNN_METAL=ON -DIOS_ARCH="armv7;armv7s" -DMNN_AAPL_FMWK=ON -DENABLE_BITCODE=0 -G Xcode
+echo "Building AArch32"
 xcodebuild ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -configuration Release -scheme MNN -target MNN -sdk iphoneos -quiet
 cd ../
 
-rm -rf iOSSIMULATOR32
-mkdir iOSSIMULATOR32
-cd iOSSIMULATOR32
-cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../..//cmake/ios.toolchain.cmake -DMNN_METAL=ON -DPLATFORM=SIMULATOR -DENABLE_BITCODE=0 -G Xcode
-echo "Building for 32Bit Simulator"
-xcodebuild ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -configuration Release -scheme MNN -target MNN -sdk iphoneos -quiet
+rm -rf SIM_32
+mkdir SIM_32
+cd SIM_32
+cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../../cmake/ios.toolchain.cmake -DMNN_METAL=ON -DPLATFORM=SIMULATOR -DENABLE_BITCODE=0 -DMNN_AAPL_FMWK=ON -G Xcode
+echo "Building Simulator32"
+xcodebuild ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -configuration Release -scheme MNN -target MNN -sdk iphonesimulator -quiet
 cd ../
+
+rm -rf SIM_64
+mkdir SIM_64
+cd SIM_64
+cmake ../../../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=../../../cmake/ios.toolchain.cmake -DMNN_METAL=ON -DPLATFORM=SIMULATOR64 -DENABLE_BITCODE=0 -DMNN_AAPL_FMWK=ON -G Xcode
+echo "Building Simulator64"
+xcodebuild ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -configuration Release -scheme MNN -target MNN -sdk iphonesimulator -quiet
+cd ../
+
+
 
 echo "Moving Slices"
-mv iOSCOMBINED/Release-iphoneos/MNN.framework/MNN ./MNN_IOS_COMBINED
-mv iOSSIMULATOR64/Release-iphoneos/MNN.framework/MNN ./MNN_iOSSIMULATOR64
-mv iOSSIMULATOR32/Release-iphoneos/MNN.framework/MNN ./MNN_iOSSIMULATOR32
-mv iOSCOMBINED/Release-iphoneos/MNN.framework output/
+rm -rf output/
+mkdir output
+mv iOS_64/Release-iphoneos/MNN.framework/MNN ./MNN_iOS64
+mv iOS_32/Release-iphoneos/MNN.framework/MNN ./MNN_iOS32
+mv SIM_32/Release-iphonesimulator/MNN.framework/MNN ./MNN_SIM_32
+mv SIM_64/Release-iphonesimulator/MNN.framework/MNN ./MNN_SIM_64
+mv iOS_32/Release-iphoneos/MNN.framework output/
 echo "Creating Fat Binary"
-lipo -create ./MNN_IOS_COMBINED ./MNN_iOSSIMULATOR64 ./MNN_iOSSIMULATOR32 -output output/MNN.framework/MNN
+lipo -create ./MNN_iOS64 ./MNN_iOS32 ./MNN_SIM_64 ./MNN_SIM_32 -output output/MNN.framework/MNN
 echo "Cleaning up"
-rm ./MNN_IOS_COMBINED
-rm ./MNN_iOSSIMULATOR64
-rm ./MNN_iOSSIMULATOR32
+rm ./MNN_iOS64 ./MNN_iOS32 ./MNN_SIM_64 ./MNN_SIM_32
 echo "Patching framework Headers"
-cp -R ../../..//include/MNN/expr output/MNN.framework/Headers/expr
-
-
-echo "Creating Fat Binary"
-lipo -create ios_32/Release-iphoneos/MNN.framework/MNN_32 ios_64/Release-iphoneos/MNN.framework/MNN -output ios_32/Release-iphoneos/MNN.framework/MNN
-rm ios_32/Release-iphoneos/MNN.framework/MNN_32
-echo "Patching Framework Headers"
-rm -rf ./MNN.framework
-cp -R ios_32/Release-iphoneos/MNN.framework ./MNN.framework
-cp -R ../../include/MNN/expr ./MNN.framework/Headers/expr
+cp -R ../../include/MNN/expr output/MNN.framework/Headers/expr
+echo "Unified Framework built at ${PWD}/output/"
 
 popd
