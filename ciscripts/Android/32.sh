24b1832452fce6f463a9ad332b6f681c5c3bc02b
海境 <haijing.zhs@alibaba-inc.com>
2020-01-14 17:30:40 +0800

    Fix OpenGL Building

--- ciscripts/Android/32.sh
+++ ciscripts/Android/32.sh
@@ -1,6 +1,8 @@
 set -e
 schema/generate.sh
 cd project/android
+rm -rf build_32
 mkdir build_32
 cd build_32
-../build_32.sh -DMNN_VULKAN=ON -DMNN_OPENMP=OFF -DMNN_USE_THREAD_POOL=ON
+cmake -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK/build/cmake/android.toolchain.cmake -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a" -DANDROID_STL=c++_static -DCMAKE_BUILD_TYPE=Release -DANDROID_NATIVE_API_LEVEL=android-21  -DANDROID_TOOLCHAIN=clang -DMNN_BUILD_FOR_ANDROID_COMMAND=true -DNATIVE_LIBRARY_OUTPUT=. -DNATIVE_INCLUDE_OUTPUT=. -DMNN_VULKAN=ON -DMNN_OPENMP=OFF -DMNN_USE_THREAD_POOL=ON -DMNN_OPENGL=ON -DMNN_OPENCL=ON ../../../
+make -j8
