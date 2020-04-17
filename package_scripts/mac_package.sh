06b9120d820c497835f24e1d1d1eafa4e123a9ec
和彬 <MNNTeam@service.taobao.com>
2020-04-17 18:03:34 +0800

    re-design package directory structure, mac add MNN.framework

--- package_scripts/mac_package.sh
+++ package_scripts/mac_package.sh
@@ -1,13 +1,22 @@
-MAC_PACKAGE_NAME=MNN_Mac_lib_and_tools
+#   MNN_Mac
+#     |------- MNN_Mac_lib
+#                   |---------- Dynamic_Library
+#                   |---------- Static_Library
+#                   |---------- MNN.framework
+#     |------- MNN_Mac_tools
+
+MAC_PACKAGE_NAME="MNN_Mac"
 
 # clear and create package directory
 ./schema/generate.sh
 MAC_PACKAGE_PATH=$(pwd)/$MAC_PACKAGE_NAME
 rm -rf $MAC_PACKAGE_PATH
 mkdir $MAC_PACKAGE_PATH && cd $MAC_PACKAGE_PATH
+mkdir MNN_Mac_lib && cd MNN_Mac_lib
 mkdir Dynamic_Library
 mkdir Static_Library
-mkdir tools
+cd ..
+mkdir MNN_Mac_tools
 cd ..
 
 rm -rf build
@@ -15,15 +24,22 @@ mkdir build && cd build
 # tools without dependency, static library without sep_build
 cmake -DCMAKE_BUILD_TYPE=Release -DMNN_BUILD_SHARED_LIBS=OFF -DMNN_SEP_BUILD=OFF -DMNN_BUILD_CONVERTER=ON -DMNN_BUILD_TRAIN=ON -DMNN_BUILD_DEMO=ON -DMNN_BUILD_QUANTOOLS=ON -DMNN_EVALUATION=ON .. && make -j8
 pushd ${MAC_PACKAGE_PATH}
-cp ../build/*.out tools
-cp ../build/MNNConvert tools
-cp ../build/MNNDump2Json tools
-cp ../build/OnnxClip tools
-cp ../build/libMNN.a Static_Library
+cp ../build/*.out MNN_Mac_tools
+cp ../build/MNNConvert MNN_Mac_tools
+cp ../build/MNNDump2Json MNN_Mac_tools
+cp ../build/OnnxClip MNN_Mac_tools
+cp ../build/libMNN.a MNN_Mac_lib/Static_Library
 popd
 
 # dynamic library without sep_build
 rm CMakeCache.txt
 cmake -DMNN_SEP_BUILD=OFF -DCMAKE_BUILD_TYPE=Release .. && make -j8
 cd ..
-cp build/libMNN.dylib ${MAC_PACKAGE_PATH}/Dynamic_Library
+cp build/libMNN.dylib ${MAC_PACKAGE_PATH}/MNN_Mac_lib/Dynamic_Library
+
+# mac framework without sep_build
+cd build
+rm CMakeCache.txt
+cmake -DMNN_SEP_BUILD=OFF -DCMAKE_BUILD_TYPE=Release -DMNN_AAPL_FMWK=ON .. && make -j8
+cd ..
+cp -r build/MNN.framework ${MAC_PACKAGE_PATH}/MNN_Mac_lib
