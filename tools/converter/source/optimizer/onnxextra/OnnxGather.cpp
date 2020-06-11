ad4e26c7897218ee5c6a8c528dae622806b7cacc
jxt1234 <jxt1234@zju.edu.cn>
2020-06-11 15:59:16 +0800

    Fix bug for onnx gather execute error

---
+++ tools/converter/source/optimizer/onnxextra/OnnxGather.cpp
@@ -0,0 +1,43 @@
+//
+//  OnnxGather.cpp
+//  MNNConverter
+//
+//  Created by MNN on 2020/06/10.
+//  Copyright © 2018, Alibaba Group Holding Limited
+//
+
+#include "MNN_generated.h"
+#include "OnnxExtraManager.hpp"
+
+namespace MNN {
+namespace Express {
+
+class OnnxGatherTransform : public OnnxExtraManager::Transform {
+public:
+    virtual EXPRP onExecute(EXPRP expr) const override {
+        auto inputs = expr->inputs();
+        int axis = 0;
+        auto op = expr->get();
+        if (nullptr != op->main_as_Extra()->attr()) {
+            for (int i=0; i<op->main_as_Extra()->attr()->size(); ++i) {
+                auto attr = op->main_as_Extra()->attr()->GetAs<Attribute>(i);
+                auto key = attr->key()->str();
+                if (key == "axis") {
+                    axis = attr->i();
+                    break;;
+                }
+            }
+        }
+        auto output = _GatherV2(inputs[0], inputs[1], _Scalar<int>(axis));
+        output->setName(expr->name());
+        return output->expr().first;
+    }
+};
+
+static auto gRegister = []() {
+    OnnxExtraManager::get()->insert("Gather", std::shared_ptr<OnnxExtraManager::Transform>(new OnnxGatherTransform));
+    return true;
+}();
+
+} // namespace Express
+} // namespace MNN
