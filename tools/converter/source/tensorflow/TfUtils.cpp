e1376e64510b80885b096a2a9a9a5df669f0f238
Evgeny Proydakov <e.proydakov@gmail.com>
2020-10-16 15:19:38 +0300

    Fixed compile warning (-Wdeprecated-declarations). Code review.

--- tools/converter/source/tensorflow/TfUtils.cpp
+++ tools/converter/source/tensorflow/TfUtils.cpp
@@ -27,8 +27,6 @@ bool tf_read_proto_from_binary(const char* filepath, google::protobuf::Message* 
 
 #if GOOGLE_PROTOBUF_VERSION >= 3011000
     codedstr.SetTotalBytesLimit(INT_MAX);
-#else
-    codedstr.SetTotalBytesLimit(INT_MAX, INT_MAX / 2);
 #endif
 
     bool success = message->ParseFromCodedStream(&codedstr);
