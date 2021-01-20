a454dd7ef5df91a20c27aa76c7474fca89ad94ac
huwei <huw@abc.com>
2021-01-20 15:35:07 +0800

    Add MNN_USE_NEON for macOS Build Failed

--- source/backend/cpu/CPUSigmoid.cpp
+++ source/backend/cpu/CPUSigmoid.cpp
@@ -11,7 +11,9 @@
 #include "backend/cpu/CPUBackend.hpp"
 #include "backend/cpu/compute/CommonOptFunction.h"
 #include "core/Macro.h"
+#ifdef MNN_USE_NEON
 #include <arm_neon.h>
+#endif
 
 namespace MNN {
 ErrorCode CPUSigmoid::onExecute(const std::vector<Tensor*>& inputs, const std::vector<Tensor*>& outputs) {
