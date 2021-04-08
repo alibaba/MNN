2fdfdcb9c3bd1bd44947f91d5aac683d53cfc5c3
弗人 <MNNTeam@service.taobao.com>
2021-04-08 20:54:30 +0800

    [PATCH 13/36] revert flatbuffers::NumToString

--- express/Expr.cpp
+++ express/Expr.cpp
@@ -21,6 +21,11 @@
 #include "cpp/ConvertToFullQuant.hpp"
 
 //#define MNN_EXPRESS_ERROR_REPORT
+static inline std::string numberToString(int index) {
+    char s[10];
+    snprintf(s, 10, "%d", index);
+    return std::string(s);
+}
 
 static bool HasUnknownDim(const std::vector<int>& dims) {
     for (const int& dim : dims) {
@@ -887,7 +892,7 @@ void Variable::save(const std::vector<VARP>& vars, NetT* dest) {
             op->inputIndexes[i] = varIndexInfo[inputExpr.first] + inputExpr.second;
         }
         if (op->name.empty()) {
-            op->name = EnumNameOpType(op->type) + flatbuffers::NumToString(index+1);
+            op->name = EnumNameOpType(op->type) + numberToString(index+1);
         }
         op->outputIndexes.resize(expr->outputSize());
         auto tensorIndexOffset = varIndexInfo[expr];
@@ -909,7 +914,7 @@ void Variable::save(const std::vector<VARP>& vars, NetT* dest) {
                 if (v == 0) {
                     dest->tensorName[subindex] = op->name;
                 } else {
-                    dest->tensorName[subindex] = op->name + flatbuffers::NumToString(v);
+                    dest->tensorName[subindex] = op->name + numberToString(v);
                 }
             }
         }
