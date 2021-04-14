7c465239793d1330c965c315c07c50bbcd84aacf
Hui Shu <MNNTeam@service.taobao.com>
2021-04-14 11:46:43 +0800

    [PATCH 28/36] Fix error message

--- pymnn/src/MNN.cc
+++ pymnn/src/MNN.cc
@@ -1036,7 +1036,7 @@ static PyObject* PyMNNInterpreter_getSessionInput(PyMNNInterpreter *self, PyObje
     Tensor *t = self->interpreter->getSessionInput(session->session, name);
     if (!t) {
         PyErr_SetString(PyExc_Exception,
-                        "PyMNNInterpreter_getSessionInput: Get output failed");
+                        "PyMNNInterpreter_getSessionInput: Get input failed");
         return NULL;
     }
 
@@ -2651,7 +2651,7 @@ PyMODINIT_FUNC MOD_INIT_FUNC(void) {
         .value("HIAI", MNN_FORWARD_USER_0)
 #endif
         .export_values();
-    
+
     using MemoryMode = BackendConfig::MemoryMode;
     using PowerMode = BackendConfig::PowerMode;
     using PrecisionMode = BackendConfig::PrecisionMode;
@@ -2687,7 +2687,7 @@ PyMODINIT_FUNC MOD_INIT_FUNC(void) {
             py::arg("backend")=MNN_FORWARD_CPU, py::arg("memory_mode")=MemoryMode::Memory_Normal,
             py::arg("power_mode")=PowerMode::Power_Normal, py::arg("precision_mode")=PrecisionMode::Precision_Normal,
             py::arg("thread_num")=1);
-    
+
     //Begin of Math OPS
     //Unary OPS
     expr_module.def("sign", &Express::_Sign);
@@ -3087,17 +3087,17 @@ PyMODINIT_FUNC MOD_INIT_FUNC(void) {
         backend_config.memory = memory_mode;
         backend_config.power = power_mode;
         backend_config.precision = precision_mode;
-        
+
         Module::BackendInfo backend_info;
         backend_info.type = backend;
         backend_info.config = &backend_config;
-        
+
         Module::Config config;
         config.dynamic = dynamic;
         config.shapeMutable = shape_mutable;
         config.rearrange = rearrange;
         config.backend = &backend_info;
-        
+
         auto converted_file_name = convertBytesEncodeIfNeed(file_name);
         auto m_ptr = Module::load(inputs, outputs, converted_file_name.data(), &config);
         if (m_ptr == nullptr) {
