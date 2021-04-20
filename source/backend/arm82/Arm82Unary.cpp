183f0f803dccd3b573b2a1485ec22895207da823
雁行 <MNNTeam@service.taobao.com>
2021-04-20 16:42:37 +0800

    [PATCH 7/7] [Arm82:Bugfix] Add HardSwish and fix NEON Bug.

--- source/backend/arm82/Arm82Unary.cpp
+++ source/backend/arm82/Arm82Unary.cpp
@@ -137,15 +137,11 @@ public:
     }
 #ifdef MNN_USE_NEON
     static float16x8_t vecFunc(const float16x8_t& x) {
-        float16x8_t value_l = vmovq_n_f16(-3);
-        float16x8_t value_h = vmovq_n_f16(3);
-        float16x8_t value_d = vmovq_n_f16(1.f/6);
-        float16x8_t value_z = vmovq_n_f16(0);
-        uint16x8_t left = vcleq_f16(x, value_l);
-        uint16x8_t right = vcgeq_f16(x, value_h);
-        float16x8_t middle = vmulq_f16(vmulq_f16(x, vaddq_f16(x, value_h)), value_d);
-        float16x8_t tmp = vbslq_f16(right, x, middle);
-        return vbslq_f16(left, tmp, value_z);
+        float16x8_t zero = vmovq_n_f16(0.f);
+        float16x8_t three = vmovq_n_f16(3.f);
+        float16x8_t six = vmovq_n_f16(6.f);
+        float16x8_t onedivsix = vmovq_n_f16(1/6.f);
+        return vmulq_f16(vmulq_f16(x, vminq_f16(vmaxq_f16(vaddq_f16(x, three), zero), six)), onedivsix);
     }
 #endif
 };
