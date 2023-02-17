7b912aacd6328837d64a2829a56299774dc7ca64
DaydreamCoding <gongqiutang@gmail.com>
2023-02-17 10:06:43 +0800

    Optimize Pipeline command by cache

--- source/core/Pipeline.cpp
+++ source/core/Pipeline.cpp
@@ -929,6 +929,16 @@ ErrorCode Pipeline::allocMemory(bool firstMalloc) {
         }
     }
     mBackend->onResizeEnd();
+
+    // cached
+    command_cached.clear();
+    for (auto& info : mInfo.second) {
+        auto& buffer = info.executeBuffer;
+        for (auto& cmdP : buffer.command) {
+            command_cached.push_back(cmdP.get());
+        }
+    }
+
     return NO_ERROR;
 }
 
@@ -955,9 +965,8 @@ ErrorCode Pipeline::execute() {
     auto& mBackend = mInfo.first.cache.first;
     auto& mBackupBackend = mInfo.first.cache.second;
     mBackend->onExecuteBegin();
-    for (auto& info : mInfo.second) {
-        auto& buffer = info.executeBuffer;
-        for (auto& cmdP : buffer.command) {
+    if (!command_cached.empty()) {
+        for (auto& cmdP : command_cached) {
             auto& cmd = *cmdP;
             // MNN_PRINT("before run: %p \n", cmd.info.get());
             // MNN_PRINT("before run: %s \n", cmd.info->name().c_str());
@@ -965,6 +974,20 @@ ErrorCode Pipeline::execute() {
             if (NO_ERROR != code) {
                 mBackend->onExecuteEnd();
                 return code;
+            }
+        }
+    } else {
+        for (auto& info : mInfo.second) {
+            auto& buffer = info.executeBuffer;
+            for (auto& cmdP : buffer.command) {
+                auto& cmd = *cmdP;
+                // MNN_PRINT("before run: %p \n", cmd.info.get());
+                // MNN_PRINT("before run: %s \n", cmd.info->name().c_str());
+                auto code = cmd.execution->onExecute(cmd.workInputs, cmd.outputs);
+                if (NO_ERROR != code) {
+                    mBackend->onExecuteEnd();
+                    return code;
+                }
             }
         }
     }
