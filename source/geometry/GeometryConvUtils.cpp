9b39b3c06c1f35935596242c327ff82d08e46995
xinhao.zheng <xinhao.zheng@arm.com>
2025-01-21 12:18:07 +0800

    Little refine for KAI_CONV_NCHW_IN_OUT implementation

--- source/geometry/GeometryConvUtils.cpp
+++ source/geometry/GeometryConvUtils.cpp
@@ -250,10 +250,7 @@ bool GeometryConvUtils::computeSingle(const Op* op, const std::vector<Tensor*>& 
 #if KAI_CONV_NCHW_IN_OUT
     KleidiAI& kai = KleidiAI::getInstance();
     if(kai.canAccelerate()) {
-        TensorUtils::getDescribe(inputs[0])->dimensionFormat = MNN_DATA_FORMAT_NCHW;
-        TensorUtils::getDescribe(outputs[0])->dimensionFormat = MNN_DATA_FORMAT_NCHW;
         kai.setLinear(true);
-
         std::shared_ptr<Command> cmd(new Command);
         cmd->op      = op;
         cmd->inputs  = std::move(inputs);
