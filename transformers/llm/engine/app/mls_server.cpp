ba11ac14671da1b5537c4efded38b01bb5ec3904
若遗 <ruoyi.sjd@alibaba-inc.com>
2025-02-17 19:47:56 +0800

    add server stream support compatible with openai api

--- transformers/llm/engine/app/mls_server.cpp
+++ transformers/llm/engine/app/mls_server.cpp
@@ -5,11 +5,9 @@
 
 #include "mls_server.hpp"
 #include <iostream>
-#include "httplib.h"
-#include "jsonhpp/json.hpp"
 
-using nlohmann::json;
-using PromptItem = std::pair<std::string, std::string>;
+
+
 
 namespace mls {
 
@@ -69,23 +67,72 @@ void Answer(MNN::Transformer::Llm* llm, const json &messages, std::function<void
   llm->response(prompts, &output_ostream, "<eop>");
 }
 
+void MlsServer::AnswerStreaming(MNN::Transformer::Llm* llm,
+                     const json& messages,
+                     std::function<void(const std::string&, bool end)> on_partial) {
+    std::vector<PromptItem> prompts;
+    if (messages.is_array()) {
+        for (const auto& item_json : messages) {
+            PromptItem item;
+            if (!FromJson(item_json, item)) {
+                std::cerr << "Error converting JSON object to PromptItem." << std::endl;
+                return;
+            }
+            prompts.push_back(item);
+        }
+    }
+    std::string answer = "";
+    Utf8StreamProcessor processor([&on_partial, &answer](const std::string &utf8Char) {
+        bool is_eop = (utf8Char.find("<eop>") != std::string::npos);
+        if (is_eop) {
+            std::string response_result = answer;
+            std::cout<<"response result: "<<response_result<<std::endl;
+            on_partial("", true);
+        } else {
+            answer += utf8Char;
+            on_partial(utf8Char, false);
+        }
+    });
+
+    // LlmStreamBuffer calls our lambda as new bytes arrive from the LLM
+    LlmStreamBuffer stream_buffer([&processor](const char* str, size_t len) {
+        processor.processStream(str, len);
+    });
+    std::ostream output_ostream(&stream_buffer);
+    std::lock_guard<std::mutex> lock(llm_mutex_);
+    llm->response(prompts, &output_ostream, "<eop>");
+}
+
+void AllowCors(httplib::Response& res) {
+    res.set_header("Access-Control-Allow-Origin",  "*");
+    res.set_header("Access-Control-Allow-Methods",  "GET, POST, PUT, DELETE, OPTIONS");
+    res.set_header("Access-Control-Allow-Headers",  "Content-Type, Authorization");
+}
+
 void MlsServer::Start(MNN::Transformer::Llm* llm) {
     // Create a server instance
     httplib::Server server;
 
     // Define a route for the GET request on "/"
     server.Get("/", [this](const httplib::Request& req, httplib::Response& res) {
+        AllowCors(res);
         res.set_content(html_content, "text/html");
     });
     server.Post("/reset", [&](const httplib::Request &req, httplib::Response &res) {
       printf("POST /reset\n");
+      AllowCors(res);
       llm->reset();
       res.set_content("{\"status\": \"ok\"}", "application/json");
     });
-    server.Post("/v1/chat/completions", [&](const httplib::Request &req, httplib::Response &res) {
-      printf("POST /v1/chat/completions\n");
+    server.Options("/chat/completions", [](const httplib::Request& /*req*/, httplib::Response& res) {
+        AllowCors(res);
+        res.status  = 200;
+    });
+    server.Post("/chat/completions", [&](const httplib::Request &req, httplib::Response &res) {
+        std::cout << "POST /chat/completions, handled by thread: "
+                << std::this_thread::get_id() << std::endl;
+      AllowCors(res);
       if (!json::accept(req.body)) {
-          // Invalid JSON
           json err;
           err["error"] = "Invalid JSON in request body.";
           res.status = 400;
@@ -94,39 +141,78 @@ void MlsServer::Start(MNN::Transformer::Llm* llm) {
       }
       json request_json = json::parse(req.body, nullptr, false);
       json messages = request_json["messages"];
+      std::cout<<"received messages:"<<messages.dump(0)<<std::endl;
       std::string model = request_json.value("model", "undefined-model");
-      Answer(llm, messages, [&res, model](const std::string& answer) {
-          json response_json = {
-          {"id", "chatcmpl" + GetCurrentTimeAsString()},
-          {"object", "chat.completion"},
-          {"created",  static_cast<int>(time(nullptr))},
-          {"model", model},
-          {
-            "choices", json::array({
+      bool stream = request_json.value("stream", false);
+      if (!stream) {
+          Answer(llm, messages, [&res, model](const std::string& answer) {
+              json response_json = {
+              {"id", "chatcmpl" + GetCurrentTimeAsString()},
+              {"object", "chat.completion"},
+              {"created",  static_cast<int>(time(nullptr))},
+              {"model", model},
               {
-                {"index", 0},
-                {
-                  "message", {
-                    {"role", "assistant"},
-                    {"content", answer}
+                "choices", json::array({
+                  {
+                    {"index", 0},
+                    {
+                      "message", {
+                        {"role", "assistant"},
+                        {"content", answer}
+                      }
+                    },
+                    {"finish_reason", "stop"}
                   }
-                },
-                {"finish_reason", "stop"}
+                })
+              },
+              {
+                "usage", {
+                  {"prompt_tokens", 10},
+                  {"completion_tokens", 7},
+                  {"total_tokens", 17}
+                }
               }
-            })
-          },
-          {
-            "usage", {
-              {"prompt_tokens", 10},
-              {"completion_tokens", 7},
-              {"total_tokens", 17}
+            };
+            res.set_content(response_json.dump(), "application/json");
+          });
+          return;
+      }
+      res.set_header("Content-Type", "text/event-stream");
+      res.set_header("Cache-Control", "no-cache");
+      res.set_header("Connection", "keep-alive");
+      res.set_chunked_content_provider(
+            "text/event-stream",
+            [llm, messages, model, this](size_t /*offset*/, httplib::DataSink &sink) {
+                auto sse_callback = [&, this](const std::string &partial_text, bool end) {
+                    std::string finish_reason = end ? "stop" : "";
+                    json sse_json = {
+                        {"id",       "chatcmpl-" + GetCurrentTimeAsString()},
+                        {"object",   "chat.completion.chunk"},
+                        {"created",  static_cast<int>(std::time(nullptr))},
+                        {"model",    model},
+                        {"choices",  json::array({
+                            {
+                                {"delta", {{"content", partial_text}}},
+                                {"index", 0},
+                                {"finish_reason", finish_reason}
+                            }
+                        })}
+                    };
+                    std::string chunk_str = "data: " + sse_json.dump() + "\n\n";
+                    sink.os.write(chunk_str.c_str(), chunk_str.size());
+                    sink.os.flush();
+                };
+                AnswerStreaming(llm, messages, sse_callback);
+                std::string done_str = "data: [DONE]\n\n";
+                sink.os.write(done_str.c_str(), done_str.size());
+                sink.os.flush();
+                sink.done();
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                return false;
             }
-          }
-        };
-        res.set_content(response_json.dump(), "application/json");
-      });
+        );
     });
-    // Start the server on port 8080
+    // Start the server on port
     std::cout << "Starting server on http://localhost:9090\n";
     if (!server.listen("0.0.0.0", 9090)) {
         std::cerr << "Error: Could not start server.\n";
