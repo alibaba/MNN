95dde21a1e05a738f66762c6f8f5130cd7544ab9
游薪渝(揽清) <azure.yxy@alibaba-inc.com>
2025-08-01 11:32:08 +0800

    [add] chat notification

---
+++ apps/iOS/MNNLLMChat/MNNLLMiOS/Util/NotificationNames.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationNames.swift
+//  MNNLLMiOS
+//
+//  Created by 游薪渝(揽清) on 2025/8/1.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static let dismissKeyboard = Notification.Name("dismissKeyboard")
+    
+    static let messageSent = Notification.Name("messageSent")
+    
+    static let onScrollToBottom = Notification.Name("onScrollToBottom")
+}
