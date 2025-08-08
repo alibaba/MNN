e07b695088c318accb918c71091f3b0e92c1d568
ihb2032 <1355790728@qq.com>
2025-08-08 08:42:09 +0800

    Unified variable naming
    Signed-off-by: lyd1992 <liuyudong@iscas.ac.cn>
    
    Signed-off-by: ihb2032 <1355790728@qq.com>

--- source/backend/cpu/riscv/rvv/MNNMatrixProd.cpp
+++ source/backend/cpu/riscv/rvv/MNNMatrixProd.cpp
@@ -6,21 +6,21 @@ void MNNMatrixProd0(float* C, const float* A, const float* B,
     size_t total = widthC4 * 4;
 
     for (int y = 0; y < height; ++y) {
-        const float* a_ptr = A + aStride * y;
-        const float* b_ptr = B + bStride * y;
-        float* c_ptr = C + cStride * y;
+        const float* a = A + aStride * y;
+        const float* b = B + bStride * y;
+        float* c = C + cStride * y;
         size_t n = total;
 
         while (n > 0) {
             size_t vl = __riscv_vsetvl_e32m8(n);
-            vfloat32m8_t va = __riscv_vle32_v_f32m8(a_ptr, vl);
-            vfloat32m8_t vb = __riscv_vle32_v_f32m8(b_ptr, vl);
+            vfloat32m8_t va = __riscv_vle32_v_f32m8(a, vl);
+            vfloat32m8_t vb = __riscv_vle32_v_f32m8(b, vl);
             vfloat32m8_t vc = __riscv_vfmul_vv_f32m8(va, vb, vl);
-            __riscv_vse32_v_f32m8(c_ptr, vc, vl);
+            __riscv_vse32_v_f32m8(c, vc, vl);
 
-            a_ptr += vl;
-            b_ptr += vl;
-            c_ptr += vl;
+            a += vl;
+            b += vl;
+            c += vl;
             n -= vl;
         }
     }
