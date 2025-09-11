1abdb41740b6afe65452a4d76ef64cce87f90724
游薪渝(揽清) <azure.yxy@alibaba-inc.com>
2025-09-11 14:48:12 +0800

    [feat] support useMarkdown

--- apps/iOS/MNNLLMChat/MNNLLMiOS/Chat/Models/LLMChatMessage.swift
+++ apps/iOS/MNNLLMChat/MNNLLMiOS/Chat/Models/LLMChatMessage.swift
@@ -13,8 +13,9 @@ struct LLMChatMessage {
     var sender: LLMChatUser
     let createdAt: Date
     var status: Message.Status?
-
+    var useMarkdown: Bool = true
     var text: String
+    var thinkText: String = ""
     let images: [LLMChatImage]
     let videos: [LLMChatVideo]
     let recording: Recording?
@@ -29,6 +30,8 @@ extension LLMChatMessage {
             status: status,
             createdAt: createdAt,
             text: text,
+            thinkText: thinkText,
+            useMarkdown: useMarkdown,
             attachments: images.map { $0.toChatAttachment() } + videos.map { $0.toChatAttachment() },
             recording: recording,
             replyMessage: replyMessage
