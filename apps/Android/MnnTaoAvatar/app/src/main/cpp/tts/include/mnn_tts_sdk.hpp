8072a6299a92e2aacd293e26b06b09f5243d77e6
weishan.wyf <weishan.wyf@alibaba-inc.com>
2025-12-15 17:56:32 +0800

    feat: add mnn supertonic support

--- apps/Android/MnnTaoAvatar/app/src/main/cpp/tts/include/mnn_tts_sdk.hpp
+++ apps/Android/MnnTaoAvatar/app/src/main/cpp/tts/include/mnn_tts_sdk.hpp
@@ -7,6 +7,7 @@
 
 #include "wavfile.hpp"
 #include "mnn_tts_config.hpp"
+#include "mnn_tts_impl_base.hpp"
 //#include "piper/mnn_piper_tts_impl.hpp"
 #include "bertvits2/mnn_bertvits2_tts_impl.hpp"
 
