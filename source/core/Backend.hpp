6cd5795b0fb154607ed94d911bb6c4c115f13cef
MNNSyncBot <hi@zhaode.wang>
2025-12-22 11:04:54 +0800

    Merge pull request #4009 from HenryDen/default_opt
    Add a compile option and macro to default enable kleidiAI
    
    GitOrigin-RevId: d252203d159374844e90bfe13589b9c0c36f62ee

--- source/core/Backend.hpp
+++ source/core/Backend.hpp
@@ -68,9 +68,11 @@ struct RuntimeHint {
 
     // whether to use Arm sme2 cores when threads>1
     bool useArmSme2Cores = true;
-
+#ifdef MNN_DEFAULT_USE_KLEIDIAI
+    bool enableKleidiAI = true;
+#else   
     bool enableKleidiAI = false;
-
+#endif
     // Use CPU Ids
     std::vector<int> cpuIds;
     
