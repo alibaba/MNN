3bab7774765d4ce3b2b136de4791cd3e1f6d6ecb
MNNSyncBot <hi@zhaode.wang>
2026-02-02 18:09:47 +0800

    [LLM:Feature] LoRA model support clone LayerNorm from base.
    Discussed-in: Merge-Request 25804622 , URL: https://code.alibaba-inc.com/AliNN/AliNNPrivate/codereview/25804622
    GitOrigin-RevId: 7ebc965b93a9fd1e8a54f53c05bc6bdb37a432b4

--- express/module/StaticModule.cpp
+++ express/module/StaticModule.cpp
@@ -149,13 +149,29 @@ static std::vector<std::shared_ptr<BufferStorage>> preRearrangeWeights( // NOLIN
                 break;
             }
             case MNN::OpType_LayerNorm: {
-                std::shared_ptr<BufferStorage> tmpstorage;
-                exe.reset(OpCommonUtils::createExecutionWithExternal(backend, info.inputs, info.outputs, op, &loader, tmpstorage));
-                if (exe.get() == nullptr) {
-                    exe.reset(OpCommonUtils::createExecutionWithExternal(backupBackend, info.inputs, info.outputs, op, &loader, tmpstorage));
+                if (!base_executions.empty() && op->name()) {
+                    auto iter = base_executions.find(op->name()->str());
+                    if (iter != base_executions.end()) {
+                        auto base_exe = iter->second.get();
+                        Execution* copyExecution = nullptr;
+                        base_exe->onClone(backend, op, &copyExecution);
+                        if (copyExecution == nullptr) {
+                            base_exe->onClone(backupBackend, op, &copyExecution);
+                        }
+                        if (copyExecution != nullptr && copyExecution->onClone(nullptr, op, nullptr)) {
+                            exe.reset(copyExecution);
+                        }
+                    }
                 }
-                if (nullptr == exe) {
-                    break;
+                if (exe == nullptr) {
+                    std::shared_ptr<BufferStorage> tmpstorage;
+                    exe.reset(OpCommonUtils::createExecutionWithExternal(backend, info.inputs, info.outputs, op, &loader, tmpstorage));
+                    if (exe.get() == nullptr) {
+                        exe.reset(OpCommonUtils::createExecutionWithExternal(backupBackend, info.inputs, info.outputs, op, &loader, tmpstorage));
+                    }
+                    if (nullptr == exe) {
+                        break;
+                    }
                 }
                 // The exe can't clone
                 if (!exe->onClone(nullptr, op, nullptr)) {
