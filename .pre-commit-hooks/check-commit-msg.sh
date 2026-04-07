c35f14f3ab5cb65094863b9a0e888370b027a670
MNNSyncBot <hi@zhaode.wang>
2026-04-07 16:47:02 +0800

    [MNN:Release] Release 3.5.0
    Discussed-in: Merge-Request 26737301 , URL: https://code.alibaba-inc.com/AliNN/AliNNPrivate/codereview/26737301
    GitOrigin-RevId: b06f3652a063f2c2b8c7016ee86081a9ea6c4944

--- .pre-commit-hooks/check-commit-msg.sh
+++ .pre-commit-hooks/check-commit-msg.sh
@@ -10,12 +10,12 @@ if printf '%s\n' "$MSG" | grep -qE "^Merge "; then
 fi
 
 # Check format: [Module:Type] Description
-if ! printf '%s\n' "$MSG" | grep -qE '^\[[A-Za-z0-9]+:(Feature|Bugfix|Perf|Refact|Style|Doc|Test|Chore)\] .+'; then
+if ! printf '%s\n' "$MSG" | grep -qE '^\[[A-Za-z0-9]+:(Feature|Bugfix|Perf|Refact|Style|Doc|Test|Chore|Release)\] .+'; then
     echo "ERROR: Commit message format must be:"
     echo "  [Module:Type] Description"
     echo ""
     echo "  Module: LLM, CPU, Metal, CUDA, OpenCL, Vulkan, Core, Infra, Doc, etc."
-    echo "  Type:   Feature, Bugfix, Perf, Refact, Style, Doc, Test, Chore"
+    echo "  Type:   Feature, Bugfix, Perf, Refact, Style, Doc, Test, Chore, Release"
     echo ""
     echo "  Example: [LLM:Feature] Add streaming support"
     echo "  Got:     $MSG"
