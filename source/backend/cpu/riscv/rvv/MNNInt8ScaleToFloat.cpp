b0c0dc7cde6bb5a5fc24f799d79bc937806baa29
Sherlockzhangjinge <zjgzhangjinge@outlook.com>
2026-05-18 18:47:12 +0800

    [CPU:Bugfix] supplement missing headfile, and fix a bug in MNNLineDepthWiseInt8AddBiasScaleUnit which causse the error below:
    不能在初始化时将 const int32_t* const 转换为 const float*
    
    Signed-off-by: Jixingguang <1955992348@qq.com>
    
    Signed-off-by: Sherlockzhangjinge <zjgzhangjinge@outlook.com>
    
    Signed-off-by: lyd1992 <liuyudong@iscas.ac.cn>

--- source/backend/cpu/riscv/rvv/MNNInt8ScaleToFloat.cpp
+++ source/backend/cpu/riscv/rvv/MNNInt8ScaleToFloat.cpp
@@ -9,6 +9,7 @@
 #include <riscv_vector.h>
 #include <stdint.h>
 #include <string.h>
+#include "../../compute/Int8FunctionsOpt.h"
 
 void MNNInt8ScaleToFloat_RVV(float* dst, const int8_t* src, const float* scale, size_t size, const float* zeroPoint,
                              ssize_t quantParamVec) {
