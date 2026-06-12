9caee8b94691a567e118e4732256ee0875bb74a0
ihb2032 <hebome@foxmail.com>
2026-06-12 13:28:22 +0800

    [CPU:Bugfix] fix rvv pack and unpack functions errors
    Co-authored-by: lyd1992 <liuyudong@iscas.ac.cn>

--- source/backend/cpu/riscv/rvv/MNNPackC2.cpp
+++ source/backend/cpu/riscv/rvv/MNNPackC2.cpp
@@ -1,74 +1,38 @@
 #include <riscv_vector.h>
 
-void MNNPackC2(float *dst, const float *src, size_t area, size_t depth, int *areaOffset) {
-    int depthC2 = depth / 2;
-    int depthRemain = depthC2 * 2;
-    int remain = depth - depthRemain;
-    const float *srcOffset = src;
-    const float *srcChannel[2];
+void MNNPackC2(float* dst, const float* src, size_t area, size_t depth, int* areaOffset) {
+    const size_t srcAreaStride = (size_t)areaOffset[0];
+    const size_t dstAreaStride = (size_t)areaOffset[1];
+    const ptrdiff_t dstStrideBytes = 2 * (ptrdiff_t)sizeof(float);
+    const size_t depthC2 = (depth + 1) / 2;
 
-    for (int z = 0; z < depthC2; ++z) {
-        float *dstZ = dst + z * areaOffset[1] * 2;
+    for (size_t z = 0; z < depthC2; ++z) {
+        const size_t cBase = z * 2;
+        float* dstZ = dst + z * dstAreaStride * 2;
+        size_t valid = depth - cBase;
+        if (valid > 2)
+            valid = 2;
 
-        for (int y = 0; y < 2; ++y) {
-            srcChannel[y] = srcOffset + areaOffset[0] * y;
-        }
+        for (size_t y = 0; y < valid; ++y) {
+            const float* srcChannel = src + (cBase + y) * srcAreaStride;
 
-        size_t x = 0;
-        size_t vl = __riscv_vsetvl_e32m8(area);
-
-        for (; x + vl <= area; x += vl) {
-            float *dstPtr = dstZ + x * 2;
-            vfloat32m8_t vec = __riscv_vle32_v_f32m8(srcChannel[0] + x, vl);
-            __riscv_vsse32_v_f32m8(dstPtr + 0, 2 * sizeof(float), vec, vl);
-            vec = __riscv_vle32_v_f32m8(srcChannel[1] + x, vl);
-            __riscv_vsse32_v_f32m8(dstPtr + 1, 2 * sizeof(float), vec, vl);
-        }
-
-        for (; x < area; ++x) {
-            float *dstPtr = dstZ + x * 2;
-            dstPtr[0] = srcChannel[0][x];
-            dstPtr[1] = srcChannel[1][x];
-        }
-
-        srcOffset += areaOffset[0] * 2;
-    }
-
-    if (remain > 0) {
-        float *dstZ = dst + depthC2 * areaOffset[1] * 2;
-
-        for (int y = 0; y < remain; ++y) {
-            srcChannel[y] = srcOffset + areaOffset[0] * y;
-        }
-
-        size_t x = 0;
-        size_t vl = __riscv_vsetvl_e32m8(area);
-
-        for (; x + vl <= area; x += vl) {
-            float *dstPtr = dstZ + x * 2;
-
-            for (int y = 0; y < remain; ++y) {
-                vfloat32m8_t vec = __riscv_vle32_v_f32m8(srcChannel[y] + x, vl);
-                __riscv_vsse32_v_f32m8(dstPtr + y, 2 * sizeof(float), vec, vl);
-            }
-
-            vfloat32m8_t zero = __riscv_vfmv_v_f_f32m8(0.0f, vl);
-            for (int y = remain; y < 2; ++y) {
-                __riscv_vsse32_v_f32m8(dstPtr + y, 2 * sizeof(float), zero, vl);
+            size_t x = 0;
+            while (x < area) {
+                const size_t vl = __riscv_vsetvl_e32m8(area - x);
+                vfloat32m8_t v = __riscv_vle32_v_f32m8(srcChannel + x, vl);
+                __riscv_vsse32_v_f32m8(dstZ + 2 * x + y, dstStrideBytes, v, vl);
+                x += vl;
             }
         }
 
-        for (; x < area; ++x) {
-            float *dstPtr = dstZ + x * 2;
-
-            for (int y = 0; y < remain; ++y) {
-                dstPtr[y] = srcChannel[y][x];
-            }
-
-            for (int y = remain; y < 2; ++y) {
-                dstPtr[y] = 0.0f;
+        for (size_t y = valid; y < 2; ++y) {
+            size_t x = 0;
+            while (x < area) {
+                const size_t vl = __riscv_vsetvl_e32m8(area - x);
+                vfloat32m8_t zero = __riscv_vfmv_v_f_f32m8(0.0f, vl);
+                __riscv_vsse32_v_f32m8(dstZ + 2 * x + y, dstStrideBytes, zero, vl);
+                x += vl;
             }
         }
     }
 }
-
